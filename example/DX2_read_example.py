8339c913bde5cbaf0151c07891b66901ead9c270
hagarlandmsan <hagar.landsman@weizmann.ac.il>
2025-04-10 11:10:22 +0300

    example script added

---
+++ example/DX2_read_example.py
@@ -0,0 +1,23 @@
+# Example1.py
+
+import DX2FileReader as DX2
+data = DX2.DX2("out_2.5GHz.V3.DX2")
+
+print (f"Got {data.Nevents}")
+Ntot = int(data.Nevents)
+
+for n in range(1,Ntot):
+    ev = data.read_event(n)
+    Nch = len(ev['waveforms'])
+    print (f"Event {n} : Sampling at {1/ev['tsamp']} GHz, time tag {ev['time_tag']}, got {Nch} channels ")
+
+    for xch in range(0, Nch):
+        wf=ev['waveforms'][xch]
+        ch_name = wf['name']
+        ch_digi = wf['pmt_ch']
+        ch_id = wf['logic_ch']
+        wf_data = wf['waveform']
+        print (f"\t Channel {xch}: {ch_name} at Digitizer={ch_digi} has {len(wf_data)} samples, with max {max(wf_data)}, and min {min(wf_data)}")
+    if n==10:
+        break
+
